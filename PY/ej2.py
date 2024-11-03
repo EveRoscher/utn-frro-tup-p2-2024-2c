8f0ffb4525bb58c845c73b860afe89156de8df86
EveRoscher <evelyncarol@hotmail.es>
2024-11-02 22:32:17 -0300

    Create ej2.py

---
+++ PY/ej2.py
@@ -0,0 +1,9 @@
+#Escribe un programa que muestre tu nombre, tu edad y tu ciudad en líneas separadas usando la función print
+
+nombre=input("Ingrese nombre ")
+edad=input("Ingrese edad ")
+ciudad=input("Ingrese ciudad ")
+
+print("\nEl nombre es:", nombre)
+print("La edad es:", edad)
+print("La ciudad es:", ciudad)
